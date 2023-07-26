c06055b35097d3234f5ff5588b75d6604803a6fd
Italo Isvi <italoisvi@hotmail.com>
2023-07-26 13:55:48 -0300

    update file countline.js

--- countlines.js
+++ countlines.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-function countLinesInFile(filePath) {
+async function countLinesInFile(filePath) {
   const content = fs.readFileSync(filePath, 'utf8');
   const lines = content.split('\n').filter(line => line.trim() !== '');
   return lines.length;
@@ -68,4 +68,34 @@ if (args.length > 0) {
   const folderPath = args[0];
   const totalLines = countLinesInFolder(folderPath);
   console.log(`Total lines in all files: ${totalLines}`);
+}
+
+function countLines() {
+  const owner = document.getElementById("owner").value;
+  const repo = document.getElementById("repo").value;
+  const token = document.getElementById("token").value;
+
+  // Faça uma solicitação HTTP para a API do GitHub
+  const response = fetch(`https://api.github.com/repos/${owner}/${repo}/contents`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  // Processe a resposta HTTP
+  response.json().then(data => {
+    // Obtenha o número de linhas de código
+    let totalLines = 0;
+    for (const file of data) {
+      if (file.type === "file") {
+        const filePath = file.download_url;
+        const lines = await countLinesInFile(filePath);
+        totalLines += lines;
+      }
+    }
+
+    // Exiba o número de linhas de código em uma janela popup
+    const popup = window.open('', 'popup', 'width=500,height=200');
+    popup.document.write(`Total lines in all files: ${totalLines}`);
+  });
 }
